ea1a93eea533e854448f7a98eefb4f85ea01ea95
Jeff Muizelaar <jmuizelaar@mozilla.com>
2013-02-28 11:09:27 -0500

    More information

--- parse.py
+++ parse.py
@@ -52,6 +52,7 @@ for row in reader:
                 # AuthenticAMD family 16 model 6 stepping 3
                 m = re.match(r"(?P<vendor>\w+) family (?P<family>\w+) model (?P<model>\w+) stepping (?P<stepping>\w+)", cpu_model)
                 [vendor, family, model, stepping] = (m.group('vendor'), int(m.group('family')), int(m.group('model')), int(m.group('stepping')))
+        
                 if vendor == "AuthenticAMD":
                     amd += 1
                 if has_sse2(vendor, family, model, stepping):
@@ -67,9 +68,9 @@ for row in reader:
                     multicore += 1
                 if int(cores) >= 4:
                     fourcore += 1
-                total += 1
                 core_sum += int(cores)
                 core_max = max(core_max, int(cores))
+                total += 1
 
             except:
                 pass
@@ -80,20 +81,45 @@ descriptive_names = {'AuthenticAMD': {6:
         6: 'Athlon XP',
         7: 'Duron Morgan',
         8: 'Athlon (Palomino) XP/Duron',
-        10: 'Athlon MP'}
-    ,5:{8:'K6-2' }},
+        10: 'Athlon MP'},
+    5:{8:'K6-2'},
+    15:{107:'AMD64 X2'},
+    16:{2: 'Athlon II X2',
+        3: 'Phenom II X3',
+        4: 'Athlon II X4',
+        5: 'Athlon II X4',
+        6: 'AMD64 Athlon II' },
+    20:{1:'AMD64 C-50',
+        2:'AMD64 C-60'},
+    },
     'CentaurHauls': {6:
         {7:'VIA Ezra/Samuel 2'}},
     'GenuineIntel': {6:
-        {7:'Pentium III Katmai 0.25 um',
+        {
+        5:'Pentium II Deschutes 0.25 um',
+        6: 'Pentium II Mendocino 0.25 um',
+        7:'Pentium III Katmai 0.25 um',
         8:'Pentium III Coppermine 0.18 um',
         11:'Pentium III Tualatin 0.13 um',
-        5:'Pentium II Deschutes 0.25 um',
-        23:'Core 2 Duo',
+        13:'Pentium M',
+        14:'Core Duo 65nm',
         15:'Core 2 Duo Allendale/Kentsfield 65nm',
-        42:'Core i[735]',
-        6: 'Pentium II Mendocino 0.25 um'}}
-        }
+        22:'Core based Celeron 65nm',
+        23:'Core 2 Duo 45nm',
+        28:'Atom',
+        37:'Core i[735] Westmere',
+        38:'Atom',
+        42:'Core i[735] Sandybridge',
+        58:'Core i[735] Ivybridge',
+        },
+        15: {
+            1:'Pentium 4 Willamette 180nm',
+            2:'Pentium 4 Northwood 130nm',
+            3:'Pentium 4 Prescott 90nm',
+            4:'Pentium 4 Prescott 2M 90nm',
+            6:'Pentium D',
+            }
+        }}
 def describe(vendor, family, model):
     name = ""
     if vendor in descriptive_names:
@@ -109,4 +135,5 @@ print "amd", "%s%%" % (amd*100./total)
 print "coreavg", 1.*core_sum/total, core_max
 print "mulicore", "%s%%" % (multicore*100./total)
 print "windowsxp", "%s%%" % (windowsxp*100./total)
+print "windowsxp",  (windowsxp, total)
 print "fourcore", "%s%%" % (fourcore*100./total)
